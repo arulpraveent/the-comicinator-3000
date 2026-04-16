6ccafe3b6ab8096fd2335a9c65c5a0db51cbbeee
Arul Praveen T <69880937+arulpraveent@users.noreply.github.com>
2026-04-16 10:33:03 +0530

    feat: initial commit (this time it will work i swear)

---
+++ app/src/main/java/com/deepvisiontech/thecomicinator3000/app/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.deepvisiontech.thecomicinator3000.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.deepvisiontech.thecomicinator3000.core.ui.theme.TheComicinator3000Theme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TheComicinator3000Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TheComicinator3000Theme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/deepvisiontech/thecomicinator3000/app/TheComicinator3000.kt
@@ -0,0 +1,8 @@
+package com.deepvisiontech.thecomicinator3000.app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TheComicinator3000: Application() {
+}
